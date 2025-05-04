ae53ae17a262eee238a0d7ed353fc0395952a432
Salmaan Saeed <salsaeed999@gmail.com>
2025-05-03 20:45:04 -0400

    implemented a slower moving progress bar

--- hello.asm
+++ hello.asm
@@ -2,6 +2,7 @@ section .text
     global _main
 extern _printf
 extern _system
+extern _sleep
 
 _main:
     mov eax, 0 ; Inititalize counter to 0
@@ -12,17 +13,24 @@ _main:
         mov bl, '#'
         mov msg[eax], bl
 
+        push 1
+        call _sleep
+        add esp, 4 ; clear the stack
+
+        push clear
+        call _system
+        add esp, 4 ; clear the stack
+
         push msg
         call _printf
         add esp, 4 ; clear the stack
-    
-        ;push clear
-        ;call _system
-        ;add esp, 4 ; clear the stack
+
+        push 1
+        call _sleep
+        add esp, 4 ; clear the stack
 
         pop eax ; Restore eax (for the loop)
         inc eax ; Increment counter
-
         cmp eax, 11 ; Compare counter with 11
         jbe loop
     
